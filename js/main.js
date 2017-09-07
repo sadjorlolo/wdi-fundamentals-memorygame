836a3965cfeecabc009736973b54a81c220fa6a9
Adjorlolo <n0238181@libp45p-26030dd.lm.lmig.com>
2017-09-07 14:05:21 -0400

    added reset and clear game functionality

--- js/main.js
+++ js/main.js
@@ -14,15 +14,63 @@ var cards = [{
 
 var cardsInPlay = [];
 
+var gamesPlayed;
+var matchesFound;
+
+var totalGames;
+var totalMatch;
+
+// set initial values for total games and matches found at load
+//if localstorage does not exist, set variables to 0
+//else, localstorage.
+
+if(localStorage.getItem('totalGames') === null) {
+	gamesPlayed = 0;
+}
+else {
+	gamesPlayed = localStorage.getItem('totalGames');
+}
+
+if(localStorage.getItem('totalMatch') === null) {
+	matchesFound = 0;
+}
+else {
+	matchesFound = localStorage.getItem('totalMatch');
+}
+
+
+var gamesPlayedText = document.getElementById('games-played');
+gamesPlayedText.innerHTML = gamesPlayed;
+
+
+var matchesFoundText = document.getElementById('matches-found');
+matchesFoundText.innerHTML = matchesFound;
+
+
+
 var checkForMatch = function(){
 	if(cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+		gamesPlayed++;
+		matchesFound++;
+		gamesPlayedText.innerHTML = gamesPlayed;
+		matchesFoundText.innerHTML = matchesFound;
+
+		totalGames = gamesPlayed;
+		localStorage.setItem('totalGames', totalGames);
+		totalMatch = matchesFound;
+		localStorage.setItem('totalMatch', totalMatch);
 	}
 	else {
 		alert("Sorry, try again.");
+		gamesPlayed++;
+		gamesPlayedText.innerHTML = gamesPlayed;
+
+		totalGames = gamesPlayed;
+		localStorage.setItem('totalGames', totalGames);
+
 	}
 }
-
 
 var flipCard = function() {
 
@@ -53,12 +101,24 @@ var createBoard = function () {
 
 createBoard();
 
+// Reset button //
+
 var resetPage = function() {
 	window.location.reload();
 }
 
 var resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', resetPage);
+
+// Reset localStorage and clear game //
+
+var resetGame = function() {
+	localStorage.clear();
+	window.location.reload();
+}
+
+var clearButton = document.getElementById('clear-game');
+clearButton.addEventListener('click', resetGame);
 
 
 
